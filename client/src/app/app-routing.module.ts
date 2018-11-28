af10e580f2c01afb50729d2c95601f11dc72c083
josesitanggang <jose.stnggng@gmail.com>
2018-11-29 03:36:59 +0700

    define routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { ProfileComponent } from './components/profile/profile.component';
+import { PostsComponent } from './components/posts/posts.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: PostsComponent
+  },
+  {
+    path:'posts',
+    component: PostsComponent
+  },
+  {
+    path:'/login',
+    component: LoginComponent
+  },
+  {
+    path:'/register',
+    component: RegisterComponent
+  },
+  {
+    path:'/profile',
+    component: ProfileComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
