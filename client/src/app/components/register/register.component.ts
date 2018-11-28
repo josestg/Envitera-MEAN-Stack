7edaaa46984382271a921d871461413828937d35
josesitanggang <jose.stnggng@gmail.com>
2018-11-29 03:59:05 +0700

    create registration ui

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  userData={};
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRegister(){
+    console.log(this.userData);
   }
 
 }
